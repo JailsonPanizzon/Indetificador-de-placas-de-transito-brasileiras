da24e4ac69b41bc2d284c2fac66d45ebc9a58557
JailsonPanizzon <jailsonpanizzon@alunos.utfpr.edu.br>
2019-03-26 15:16:47 -0300

    classifidcadores

--- Curva Suave/02 haarTraining.bat
+++ Curva Suave/02 haarTraining.bat
@@ -1,3 +1,3 @@
-haartraining.exe -data cascades -vec vector/vector.vec -bg negative/bg.txt -npos 374 -nneg 2198 -nstages 10 -mem 900 -mode ALL -w 50 -h 50 
+haartraining.exe -data cascades -vec vector/vector.vec -bg negative/bg.txt -npos 374 -nneg 2198 -nstages 11 -mem 900 -mode ALL -w 50 -h 50 
 
 rem -nonsym
